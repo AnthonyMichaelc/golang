33f5522ac5bcd9e7cbc9309c55137254a4911b60
Anthony <Ant.michaelc@gmail.com>
2018-06-28 23:59:59 -0400

    some refresher golang

---
+++ hello.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+  "fmt"
+// "math"
+// "math/rand"
+// "time"
+)
+// func add(x int, y int ) int {
+//     return x + y
+// }
+// Simple hello world, displays to the terminal
+// var c, python, java bool
+
+func main() {
+  var i, j int = 1, 2
+  k := 3
+  c, python, java := true, false, "no!"
+  fmt.Println(i, j, k, c, python, java)
+    // var i int
+    // fmt.Println(i, c, python, java)
+
+    // fmt.Println(add(42, 13))
+
+    // fmt.Println("Now you have problems.", math.Sqrt(9))
+    // fmt.Println("Hello, World!")
+
+    // fmt.Println("The time is", time.Now())
+    // fmt.Println("My favorite number is", rand.Intn(10))
+}
